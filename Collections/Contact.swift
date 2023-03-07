1d032b5e4240a7e85bbe7e9cc75db02a901d47ac
Danyil Marchuk <zemero58@gmail.com>
2023-03-07 01:31:34 +0100

    1) add the Contact struct 2) write logic for Dictionary storyboard

---
+++ Collections/Contact.swift
@@ -0,0 +1,12 @@
+//
+//  Contact.swift
+//  Collections
+//
+//  Created by Данік on 06/03/2023.
+//
+
+import Foundation
+struct Contact: Equatable {
+    let name: String
+    let phone: String
+}

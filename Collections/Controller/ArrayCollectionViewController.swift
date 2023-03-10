877a9cf33124a597418a6f58ff201d5ad5d31b89
Danyil Marchuk <zemero58@gmail.com>
2023-03-10 20:44:02 +0100

    change the arrayVC and add a new section to it

--- Collections/Controller/ArrayCollectionViewController.swift
+++ Collections/Controller/ArrayCollectionViewController.swift
@@ -12,26 +12,18 @@ class ArrayCollectionViewController: UIViewController, UICollectionViewDelegate,
     
     
     let tasksArr: [String] = ["Insert 1000 elements at the begining of the array one-by-one.", "Insert 1000 elements at the begining of the array.", "Insert 1000 elements in the middle of the array one-by-one.", "Insert 1000 elements in the middle of the array.", "Insert 1000 elements at the end of the array one-by-one.", "Insert 1000 elements at the end of the array.", "Remove 1000 elements at the end of the array one by-one.", "Remove 1000 elements at the end of the array.", "Remove 1000 elements at the beginning of the array one by-one.", "Remove 1000 elements at the beginning of the array.", "Remove 1000 elements in the middle of the array one by-one.", "Remove 1000 elements in the middle of the array."]
+    let createArr: [String] = ["Create an Int array with 10_000_000 elements"]
     var manager = Manager()
     var collectionView: UICollectionView?
-    let intArray = [Int](0..<10000000)
+    var intArray: [Int] = []
     let oneThousandElements = Array(0...999)
+    let section1ItemSize = CGSize(width: UIScreen.main.bounds.width, height: 100)
+    let section2ItemSize = CGSize(width: UIScreen.main.bounds.width / 2, height: 100)
+    var numberOfSections = 2
+    var numberOfItems = 0
     
     override func viewDidLoad() {
-        
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: view.frame.size.width/2, height: view.frame.size.height/8)
-        layout.minimumInteritemSpacing = 0
-        layout.minimumLineSpacing = 0.5
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView?.register(MyCollectionViewCell.nib(), forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
-        collectionView?.register(Header.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: Header.identifier)
-        collectionView?.delegate = self
-        collectionView?.dataSource = self
-        collectionView?.backgroundColor = .gray
-        view.addSubview(collectionView!)
+        layoutSetup()
         
     }
     
@@ -46,45 +38,38 @@ class ArrayCollectionViewController: UIViewController, UICollectionViewDelegate,
     
     // MARK: - CollectionView Cells
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return numberOfSections
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        tasksArr.count
+        if section == 0 {
+            return createArr.count
+        } else {
+            return numberOfItems
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
         cell.label.text = tasksArr[indexPath.row]
-        
         return cell
     }
     
-    // MARK: - CollectionView Header
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader,
-                                                                     withReuseIdentifier: Header.identifier,
-                                                                     for: indexPath) as! Header
-
-        header.configure()
-        header.isUserInteractionEnabled = true
-        return header
-        
-
-        
-    
-        
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 0 {
+            return section1ItemSize
+        } else {
+            return section2ItemSize
+        }
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: view.frame.size.width, height: 100)
-    }
     
     
 
     // MARK: - A View was clicked on
-    
-    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
+
         let cellNumber = indexPath.row
         if let cell = collectionView.cellForItem(at: indexPath) as? MyCollectionViewCell {
             // disable the button while the array is being created
@@ -94,35 +79,38 @@ class ArrayCollectionViewController: UIViewController, UICollectionViewDelegate,
             DispatchQueue.global(qos: .userInitiated).async {
                 // Get the start time
                 let start = DispatchTime.now()
-                // create an array
-                self.performOperation(cellNumber: cellNumber)
+                // if section 0 was clicked
+                if indexPath.section == 0 {
+                    self.performOperation(cellNumber: cellNumber)
+                    self.numberOfSections = 2
+                } else {
+                    // if section 1
+                    self.performOperation(cellNumber: cellNumber + 1)
+                }
                 // Get the end time
                 let end = DispatchTime.now()
                 // Calculate the execution time
                 let time = self.manager.calculateExecutionTime(end: end, start: start)
                 // Switch back to the main thread to update the UI
                 DispatchQueue.main.async {
-                    cell.label.text = self.textWithTime(cellNumber: cellNumber, time: time)
+                    if indexPath.section == 0 {
+                        cell.label.text = self.manager.textWithTime(cellNumber: cellNumber, time: time)
+                        self.numberOfItems = self.tasksArr.count
+                        let indexSet = IndexSet(integer: 1)
+                        collectionView.reloadSections(indexSet)
+                    } else {
+                        cell.label.text = self.manager.textWithTime(cellNumber: cellNumber + 1, time: time)
+                    }
+                    // set the cell's background color to white
+                    cell.backgroundColor = .white
                     // Remove the loading progress bar
                     activityIndicator.removeFromSuperview()
-                    // Enable the cell
-                    cell.isUserInteractionEnabled = true
                 }
             }
         }
         
     }
     
-    func textWithTime(cellNumber: Int, time: Double) -> String {
-        switch cellNumber {
-        case 0...5:
-            return "Insertion time \(time)"
-        case 6...11:
-            return "Removal time \(time)"
-        default:
-            return "Error"
-        }
-    }
     
     func performOperation(cellNumber: Int) {
         var copyArr = intArray
@@ -131,43 +119,59 @@ class ArrayCollectionViewController: UIViewController, UICollectionViewDelegate,
         
         switch cellNumber {
         case 0:
+            intArray = [Int](0..<10000000)
+        case 1:
             for element in oneThousandElements {
                 copyArr.insert(element, at: 0)
             }
-        case 1:
-            copyArr.insert(contentsOf: oneThousandElements, at: 0)
         case 2:
+            copyArr.insert(contentsOf: oneThousandElements, at: 0)
+        case 3:
             for (index, element) in oneThousandElements.enumerated() {
                 copyArr.insert(element, at: middleIndex + index)
             }
-        case 3:
-            copyArr.insert(contentsOf: oneThousandElements, at: middleIndex)
         case 4:
+            copyArr.insert(contentsOf: oneThousandElements, at: middleIndex)
+        case 5:
             for element in self.oneThousandElements {
                 copyArr.insert(element, at: endIndex)}
-        case 5:
-            copyArr.insert(contentsOf: oneThousandElements, at: endIndex)
         case 6:
+            copyArr.insert(contentsOf: oneThousandElements, at: endIndex)
+        case 7:
             for _ in oneThousandElements {
                 copyArr.removeLast()}
-        case 7:
-            copyArr.removeLast(1000)
         case 8:
+            copyArr.removeLast(1000)
+        case 9:
             for _ in oneThousandElements {
                 copyArr.removeFirst()
             }
-        case 9:
-            copyArr.removeFirst(1000)
         case 10:
+            copyArr.removeFirst(1000)
+        case 11:
             for (index, _) in oneThousandElements.enumerated() {
                 copyArr.remove(at: middleIndex + index)
             }
-        case 11:
+        case 12:
             copyArr.removeSubrange(middleIndex..<middleIndex + 1000)
         default:
             print("wow")
         }
         
+    }
+    
+    private func layoutSetup() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 1, right: 0)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0.5
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.register(MyCollectionViewCell.nib(), forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.backgroundColor = .gray
+        view.addSubview(collectionView!)
     }
     
     

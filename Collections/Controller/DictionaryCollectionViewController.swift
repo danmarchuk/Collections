0e4cf5809dd168d72fb188ae07882be49e79f6e3
Danyil Marchuk <zemero58@gmail.com>
2023-03-10 20:45:00 +0100

    move some code from the Dictionary VC to the Manager

--- Collections/Controller/DictionaryCollectionViewController.swift
+++ Collections/Controller/DictionaryCollectionViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class DictionaryCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
-    
-    
     let dictTasksArr: [String] = ["Find the first contact", "Find the first contact", "Find the last contact", "Find the last contact", "Search for a non-existing element", "Search for a non-existing element"]
     var contactArray: [Contact] = []
     var contactDict: [String:String] = [:]
@@ -21,25 +19,11 @@ class DictionaryCollectionViewController: UIViewController, UICollectionViewDele
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        DispatchQueue.main.async {
+        DispatchQueue.global(qos: .background).async {
             self.addContactsToArray()
             self.addContactstoDict()
         }
-        
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: view.frame.size.width/2, height: view.frame.size.height/8)
-        layout.minimumInteritemSpacing = 0
-        layout.minimumLineSpacing = 0.5
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView?.register(MyCollectionViewCell.nib(), forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
-        collectionView?.register(DictionaryHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DictionaryHeader.identifier)
-        collectionView?.delegate = self
-        collectionView?.dataSource = self
-        collectionView?.backgroundColor = .gray
-        view.addSubview(collectionView!)
-        
+        layoutSetup()
 
     }
     
@@ -57,6 +41,7 @@ class DictionaryCollectionViewController: UIViewController, UICollectionViewDele
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
         cell.label.text = dictTasksArr[indexPath.row]
+        cell.label.textAlignment = .center
         return cell
     }
     
@@ -75,13 +60,12 @@ class DictionaryCollectionViewController: UIViewController, UICollectionViewDele
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: view.frame.size.width, height: 100)
+        return CGSize(width: view.frame.size.width, height: 75)
     }
     
     
     
     // MARK: - A View was clicked on
-    
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         var elementNumber = 0
@@ -102,7 +86,7 @@ class DictionaryCollectionViewController: UIViewController, UICollectionViewDele
                 let time = self.manager.calculateExecutionTime(end: end, start: start)
                 // Switch back to the main thread to update the UI
                 DispatchQueue.main.async {
-                    cell.label.text = self.textWithTime(cellNumber: cellNumber, time: time, elementNumber: elementNumber)
+                    cell.label.text = self.manager.dictTextWithTime(cellNumber: cellNumber, time: time, elementNumber: elementNumber)
                     // Remove the loading progress bar
                     activityIndicator.removeFromSuperview()
                     // Enable the cell
@@ -150,24 +134,9 @@ class DictionaryCollectionViewController: UIViewController, UICollectionViewDele
             return 10_000_001
         default:
             print("Hello World")
-            
         }
         return 0
     }
-    func textWithTime(cellNumber: Int, time: Double, elementNumber: Int) -> String {
-        switch cellNumber {
-        case 0...1:
-            return "First element search time: \(time) ms. Result Number \(elementNumber)"
-        case 2...3:
-            return "Last element search time: \(time) ms. Result Number \(elementNumber)"
-        case 4...5:
-            return "Non-existing element search time: \(time) ms. Result Number \(elementNumber)"
-        default:
-            return "Error"
-        }
-        
-    }
-    
     
     func addContactsToArray() {
         for i in 0..<10_000_000 {
@@ -183,6 +152,22 @@ class DictionaryCollectionViewController: UIViewController, UICollectionViewDele
             let phone = String(format: "%07d", i)
             contactDict[name] = phone
         }
+    }
+    
+    private func layoutSetup() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: view.frame.size.width/2 - 0.5, height: view.frame.size.height/8)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 1
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.register(MyCollectionViewCell.nib(), forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        collectionView?.register(DictionaryHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DictionaryHeader.identifier)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.backgroundColor = .gray
+        view.addSubview(collectionView!)
     }
 
 }

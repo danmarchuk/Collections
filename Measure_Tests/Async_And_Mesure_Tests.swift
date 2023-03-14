e995c54bf408077e4269384e8f3dbc45a69fa299
Danyil Marchuk <zemero58@gmail.com>
2023-03-14 19:14:38 +0100

    add more tests

---
+++ Measure_Tests/Async_And_Mesure_Tests.swift
@@ -0,0 +1,64 @@
+//
+//  Measure_Tests.swift
+//  Measure_Tests
+//
+//  Created by Данік on 14/03/2023.
+//
+
+import XCTest
+
+final class Async_And_Mesure_Tests: XCTestCase {
+    
+    let manager = Manager()
+    let contact = Contact.self
+
+    
+    func test_async_addContactsToArray() {
+        
+        let exp = expectation(description: "The contacts were added to the array")
+        
+        var arr: [Contact] = []
+        
+        DispatchQueue.main.async {
+            arr = self.manager.addContactsToArray()
+            XCTAssertNotNil(arr)
+            exp.fulfill()
+            print("Expectation was fulfilled")
+        }
+        wait(for: [exp], timeout: 10.0)
+    }
+    
+    
+    func test_async_addContactsToDict() {
+        
+        let exp = expectation(description: "The contacts were added to the dictionary")
+        
+        var dict: [String:String] = [:]
+        
+        DispatchQueue.main.async {
+            dict = self.manager.addContactstoDict()
+            XCTAssertNotNil(dict)
+            exp.fulfill()
+            print("Expectation was fulfilled")
+        }
+        wait(for: [exp], timeout: 10.0)
+    }
+    
+    
+    
+    func test_measure_addContactsToArray() {
+        var arr: [Contact] = []
+        measure {
+            arr = manager.addContactsToArray()
+        }
+    }
+    
+    
+    func test_measure_addContactsToDictionary() {
+        var dict: [String:String] = [:]
+        measure {
+            dict = manager.addContactstoDict()
+        }
+    }
+
+}
